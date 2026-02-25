f1d99a2a6c50151312a3980dfd4db42f14708ff0
Aleksandr Skuratov <askuratov@notissimus.com>
2026-02-25 17:02:07 +0300

    Обновил под .net10

--- Native/iOS/YandexMobileAdsProxy/generation.sh
+++ Native/iOS/YandexMobileAdsProxy/generation.sh
@@ -1,42 +1,9 @@
-copy_frameworks() {
-    local SOURCE_DIR=$1
-    local DEST_DIR=$2
-    local EXCLUDE_FRAMEWORK=$3
 
-    # Очищаем папку Frameworks (если существует)
-    if [ -d "$DEST_DIR" ]; then
-        rm -rf "$DEST_DIR"/*
-    else
-        mkdir -p "$DEST_DIR"
-    fi
-
-    # Копируем все .framework файлы, кроме исключенных
-    find "$SOURCE_DIR" -name "*.framework" | while read -r framework; do
-        framework_name=$(basename "$framework")
-        if [ "$framework_name" != "$EXCLUDE_FRAMEWORK" ]; then
-            cp -R "$framework" "$DEST_DIR"
-        fi
-    done
-}
-
+pod install
 
 #
 # Сборка для айфона
 #
-
-
-xcodebuild \
--workspace YandexMobileAdsProxy.xcworkspace \
--scheme YandexMobileAdsProxy \
--configuration Release \
--sdk iphoneos \
--derivedDataPath build
-
-SOURCE_DIR="build/Build/Products/Release-iphoneos"
-DEST_DIR="Frameworks"
-EXCLUDE="YandexMobileAdsProxy.framework"
-
-copy_frameworks "$SOURCE_DIR" "$DEST_DIR" "$EXCLUDE"
 
 xcodebuild archive \
 -workspace YandexMobileAdsProxy.xcworkspace \
@@ -52,19 +19,6 @@ SKIP_INSTALL=NO
 # Сборка для эмулятора
 #
 
-
-xcodebuild \
--workspace YandexMobileAdsProxy.xcworkspace \
--scheme YandexMobileAdsProxySimulator \
--configuration Release \
--sdk iphonesimulator \
--derivedDataPath build
-
-SOURCE_DIR="build/Build/Products/Release-iphonesimulator"
-DEST_DIR="Frameworks"
-EXCLUDE="YandexMobileAdsProxy.framework"
-
-copy_frameworks "$SOURCE_DIR" "$DEST_DIR" "$EXCLUDE"
 
 xcodebuild archive \
 -workspace YandexMobileAdsProxy.xcworkspace \
